730d11858b7b62339affb08bae7a164fc3e8fef6
GBTC406017UR <GBTC406017UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-05-16 14:30:20 -0400

    todo list using bootstrap

---
+++ src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,47 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Todo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=3)
+    private String title;
+
+    @Size(min=0,max=150)
+    private String comment;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
